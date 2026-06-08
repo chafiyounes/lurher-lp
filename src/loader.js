175dfeabdb3e1c52a00ac826bf1fbf91653bbcfb
youneschafi <148773350+youneschafi@users.noreply.github.com>
2026-06-08 17:43:47 +0100

    Update language images infrastructure and fix loader caching

--- src/loader.js
+++ src/loader.js
@@ -18,7 +18,7 @@
 
   var REPO = "chafiyounes/mapper-youcant";
   var BRANCH = "main";
-  var BASE = "https://cdn.jsdelivr.net/gh/" + REPO + "@" + BRANCH + "/";
+  var BASE = "https://raw.githubusercontent.com/" + REPO + "/" + BRANCH + "/";
 
   var FILES = {
     css: BASE + "src/styles.css",
